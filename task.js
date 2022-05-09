72c8fcd8cef570d33f9cc1724e297a77e63fe664
sangeetha-ram <sangiammu1020@gmail.com>
2022-05-10 05:11:29 +0530

    add script

---
+++ task.js
@@ -0,0 +1,10 @@
+var addInput= document.getElementById('input');
+var addBtn= document.getElementById('btn');
+var toDoContainer= document.getElementById('toDoContainer');
+
+addBtn.addEventListener('click',function() {
+    var paragraph=document.createElement('p'); //creating a new Element p tag,named paragraph
+    paragraph.innerText=input.value; //takes the input value and saves it in paragraph's innertext
+    console.log(input.value); 
+    toDoContainer.appendChild(paragraph); //paragraph Element is appended from toDoContainer
+})
